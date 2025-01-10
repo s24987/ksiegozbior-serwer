f6d6f5154a5da98c19f52e3cb3fc58fc318c1b22
s24987 <s24987@pjwstk.edu.pl>
2025-01-10 15:35:49 +0100

    reformat code: use bigger indent

--- routes/authors.js
+++ routes/authors.js
@@ -3,25 +3,25 @@ const router = express.Router();
 const db = require('../database');
 
 /* GET all authors */
-router.get('/', function(req, res, next) {
-  const query = 'select * from authors;';
-  db.query(query).then(([data, metadata]) => {
-    console.log(data);
-    return res.json(data);
-  })
+router.get('/', function (req, res, next) {
+    const query = 'select * from authors;';
+    db.query(query).then(([data, metadata]) => {
+        console.log(data);
+        return res.json(data);
+    })
 });
 
 /* POST a new author */
 router.post('/', function (req, res, next) {
-  const { name, birthdate } = req.body;
-  const insertQuery = 'INSERT INTO authors(name, birthdate) VALUES(?, ?);';
-  db.execute(insertQuery, [name, birthdate]).then(([result, _]) => {
-    return res.status(201).json({authorId: result.insertId});
-  })
-      .catch((err) => {
-        console.log(err);
-        return res.status(500).json({ message: err.message });
-      })
+    const {name, birthdate} = req.body;
+    const insertQuery = 'INSERT INTO authors(name, birthdate) VALUES(?, ?);';
+    db.execute(insertQuery, [name, birthdate]).then(([result, _]) => {
+        return res.status(201).json({authorId: result.insertId});
+    })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json({message: err.message});
+        })
 })
 
 module.exports = router;
