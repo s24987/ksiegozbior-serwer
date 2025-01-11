6e5816c5e0df2ef6bea44360e0e783b1f93dfb34
s24987 <s24987@pjwstk.edu.pl>
2025-01-11 16:55:43 +0100

    add library db validator

--- utils/db-data-validator.js
+++ utils/db-data-validator.js
@@ -12,3 +12,15 @@ module.exports.validateDbUser = () => [
             }
         })
 ];
+
+module.exports.validateDbLibrary = () => [
+    body()
+        .custom(async (bodyData) => {
+            const userId = bodyData.userId;
+            const bookId = bodyData.bookId;
+            const [result, _] = await db.query('SELECT COUNT(*) AS count FROM libraries WHERE user_id=? OR book_id=?', [userId, bookId]);
+            if(result[0].count > 0) {
+                throw new Error('This book is already in the database');
+            }
+        })
+];
