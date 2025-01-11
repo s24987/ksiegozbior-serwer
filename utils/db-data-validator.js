4700108878fd8256415a7962bca30bdf3a2cbff4
s24987 <s24987@pjwstk.edu.pl>
2025-01-11 16:40:27 +0100

    add user email db validation

--- utils/db-data-validator.js
+++ utils/db-data-validator.js
@@ -2,13 +2,13 @@ const {body} = require('express-validator');
 const db = require('../database');
 
 module.exports.validateDbUser = () => [
-    body('username')
-        .isString()
-        .trim()
-        .custom(async (value) => {
-            const [result, _] = await db.query('SELECT COUNT(*) AS count FROM users WHERE username=?', [value]);
+    body()
+        .custom(async (bodyData) => {
+            const username = bodyData.username;
+            const email = bodyData.email;
+            const [result, _] = await db.query('SELECT COUNT(*) AS count FROM users WHERE username=? OR email=?', [username, email]);
             if(result[0].count > 0) {
-                throw new Error('Username already exists');
+                throw new Error('Username or email already exists');
             }
         })
 ];
